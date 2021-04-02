446de6d52c97fcc156dfd21fb6f38bb8dc992af5
Mikulew <mikulew@gmail.com>
2021-04-02 19:49:13 +0200

    Restart project and install Vue CLI project

--- src/index.js
+++
@@ -1,9 +0,0 @@
-import "./styles.css";
-
-document.getElementById("app").innerHTML = `
-<h1>Music theory!</h1>
-<div>
-Music theory is the study of the practices and possibilities of music. The Oxford Companion to Music describes three interrelated uses of the term "music theory". The first is the "rudiments", that are needed to understand music notation (key signatures, time signatures, and rhythmic notation); the second is learning scholars' views on music from antiquity to the present; the third a sub-topic of musicology that "seeks to define processes and general principles in music". The musicological approach to theory differs from music analysis "in that it takes as its starting-point not the individual work or performance but the fundamental materials from which it is built."
-  <a href="https://en.wikipedia.org/wiki/Music_theory" target="_blank" rel="noopener noreferrer">Source</a>
-</div>
-`;
