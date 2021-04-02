22768ed57735611d7841cad9f169dd0c9bd63a5c
Mikulew <mikulew@gmail.com>
2021-04-02 20:16:30 +0200

    Basic layout

--- src/components/MusicTheory.vue
+++ src/components/MusicTheory.vue
@@ -1,6 +1,21 @@
 <template>
   <div>
-    {{ msg }}
+    <div>
+      <select name="type">
+        <option value="chromatic">Chromatic scale</option>
+        <option value="major">Major</option>
+        <option value="minor">Minor</option>
+        <option value="passages">Passages</option>
+        <option value="progression">Progression</option>
+      </select>
+      <select name="notation">
+        <option value="chromatic">European</option>
+        <option value="major">American</option>
+      </select>
+    </div>
+    <div>
+      C D E F G A H C
+    </div>
   </div>
 </template>
 
